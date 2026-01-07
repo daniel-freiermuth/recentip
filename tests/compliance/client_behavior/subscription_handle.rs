6053a9df16de986d544f4522b5b5e209aa101997
Daniel <d_freiermu14@cs.uni-kl.de>
2026-01-07 22:54:38 +0100

    add two more subscription handle tests

--- tests/compliance/client_behavior/subscription_handle.rs
+++ tests/compliance/client_behavior/subscription_handle.rs
@@ -17,7 +17,7 @@ impl Service for ServiceA {
 
 #[test_log::test]
 #[ignore = "documenting a known problem"]
-fn test_subscribe_drop_unsubscribes() {
+fn test_subscribe_drop_unsubscribes_in_time() {
     let mut sim = turmoil::Builder::new()
         .simulation_duration(Duration::from_secs(30))
         .build();
@@ -109,4 +109,224 @@ fn test_subscribe_drop_unsubscribes() {
         unsub_delay < delay_expectation,
         "Unsubscribe delay too high: {:?} > {:?}", unsub_delay, delay_expectation
     );
+}
+
+#[test_log::test]
+#[ignore]
+fn test_two_subscribers_one_drops() {
+    let mut sim = turmoil::Builder::new()
+        .simulation_duration(Duration::from_secs(30))
+        .build();
+
+    sim.host("server", move || {
+        async move {
+            let runtime: TurmoilRuntime =
+                Runtime::with_socket_type(Default::default()).await.unwrap();
+
+            // Offer instance 1
+            let mut offering = runtime
+                .offer::<ServiceA>(InstanceId::Id(0x0001))
+                .udp()
+                .start()
+                .await
+                .unwrap();
+
+            loop {
+                tokio::time::sleep(Duration::from_secs(1)).await;
+                tracing::info!("Server: Sending event");
+                offering
+                    .notify(
+                        EventgroupId::new(1).unwrap(),
+                        EventId::new(0x8001).unwrap(),
+                        "test".as_bytes(),
+                    )
+                    .await
+                    .unwrap();
+                if let Some(event) = offering.next().await {
+                    match event {
+                        someip_runtime::ServiceEvent::Unsubscribe { .. } => {
+                            tracing::info!("Server: Received unsubscribe");
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+    });
+
+    sim.client("client", async move {
+        let runtime_config = RuntimeConfigBuilder::default()
+            .advertised_ip(turmoil::lookup("client").to_string().parse().unwrap())
+            .build();
+        let runtime: TurmoilRuntime = Runtime::with_socket_type(runtime_config).await.unwrap();
+
+        let service = runtime
+            .find::<ServiceA>(InstanceId::new(1).unwrap())
+            .available()
+            .await
+            .unwrap();
+
+        let flow1 = async {
+            let mut subscription = service
+                .subscribe(EventgroupId::new(1).unwrap())
+                .await
+                .unwrap();
+
+            let now = tokio::time::Instant::now();
+            while now.elapsed() < Duration::from_secs(5) {
+                let _event = subscription.next().await.unwrap();
+                tracing::info!("Received event in flow 1");
+            }
+
+            drop(subscription);
+        };
+        let flow2 = async {
+            let mut subscription = service
+                .subscribe(EventgroupId::new(1).unwrap())
+                .await
+                .unwrap();
+
+            let mut event_count = 0;
+            let now = tokio::time::Instant::now();
+            while now.elapsed() < Duration::from_secs(15) {
+                let _event = subscription.next().await.unwrap();
+                tracing::info!("Received event in flow 1");
+                event_count += 1;
+            }
+            assert!(event_count > 14, "Did not receive enough events: {}", event_count);
+        };
+
+        let _t = tokio::join!(flow1, flow2);
+
+        runtime.shutdown().await;
+
+        Ok(())
+    });
+
+    sim.run().unwrap();
+}
+
+#[test_log::test]
+#[ignore]
+fn test_dangling_subscription_cannot_unsubscribe() {
+    let mut sim = turmoil::Builder::new()
+        .simulation_duration(Duration::from_secs(30))
+        .build();
+
+    sim.host("server", move || {
+        async move {
+            let runtime: TurmoilRuntime =
+                Runtime::with_socket_type(Default::default()).await.unwrap();
+
+            // Offer instance 1
+            let mut offering = runtime
+                .offer::<ServiceA>(InstanceId::Id(0x0001))
+                .udp()
+                .start()
+                .await
+                .unwrap();
+
+            for _ in 0..10 {
+                tokio::time::sleep(Duration::from_secs(1)).await;
+                tracing::info!("Server: Sending event");
+                offering
+                    .notify(
+                        EventgroupId::new(1).unwrap(),
+                        EventId::new(0x8001).unwrap(),
+                        "test".as_bytes(),
+                    )
+                    .await
+                    .unwrap();
+                if let Some(event) = offering.next().await {
+                    match event {
+                        someip_runtime::ServiceEvent::Unsubscribe { .. } => {
+                            tracing::info!("Server: Received unsubscribe");
+                        }
+                        _ => {}
+                    }
+                }
+            }
+
+            drop(offering);
+            tokio::time::sleep(Duration::from_secs(5)).await;
+
+            let mut offering2 = runtime
+                .offer::<ServiceA>(InstanceId::Id(0x0001))
+                .udp()
+                .start()
+                .await
+                .unwrap();
+
+            loop {
+                tokio::time::sleep(Duration::from_secs(1)).await;
+                tracing::info!("Server: Sending event");
+                offering2
+                    .notify(
+                        EventgroupId::new(1).unwrap(),
+                        EventId::new(0x8001).unwrap(),
+                        "test".as_bytes(),
+                    )
+                    .await
+                    .unwrap();
+                if let Some(event) = offering2.next().await {
+                    match event {
+                        someip_runtime::ServiceEvent::Unsubscribe { .. } => {
+                            tracing::info!("Server: Received unsubscribe");
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+    });
+
+    sim.client("client", async move {
+        let runtime_config = RuntimeConfigBuilder::default()
+            .advertised_ip(turmoil::lookup("client").to_string().parse().unwrap())
+            .build();
+        let runtime: TurmoilRuntime = Runtime::with_socket_type(runtime_config).await.unwrap();
+
+        let service = runtime
+            .find::<ServiceA>(InstanceId::new(1).unwrap())
+            .available()
+            .await
+            .unwrap();
+
+        let mut subscription1 = service
+            .subscribe(EventgroupId::new(1).unwrap())
+            .await
+            .unwrap();
+
+        while let Some(_) = tokio::time::timeout(Duration::from_secs(2), subscription1.next()).await.expect("This should not timeout. The server sends events") {
+            tracing::info!("Received event for sub1");
+        }
+        tracing::info!("Sub1 seems dead");
+
+        // What's the semantics here? The service went offline? Actually, this API design might be a bit weird.
+        let mut subscription2 = service
+            .subscribe(EventgroupId::new(1).unwrap())
+            .await
+            .unwrap();
+
+        let mut event_count = 0;
+        let now = tokio::time::Instant::now();
+        while now.elapsed() < Duration::from_secs(5) {
+            let _event = subscription2.next().await.unwrap();
+            tracing::info!("Received event in flow 1");
+            event_count += 1;
+        }
+        drop(subscription1);
+        while now.elapsed() < Duration::from_secs(10) {
+            let _event = subscription2.next().await.unwrap();
+            tracing::info!("Received event in flow 1");
+            event_count += 1;
+        }
+        assert!(event_count > 8, "Did not receive enough events: {}", event_count);
+
+        runtime.shutdown().await;
+
+        Ok(())
+    });
+
+    sim.run().unwrap();
 }
